afa8657e82ea9a085b335cd22cee0ca6480aad47
Benedikt Kretzmeyer <benedikt.kretzmeyerni-oldenburg.de>
2019-12-13 17:31:09 +0100

    add training

--- gap_size_list.m
+++ gap_size_list.m
@@ -11,7 +11,7 @@ cd('C:\Users\zuse\OneDrive\Masterarbeit\Code\')
 % rows in position matrix:
 
 % self driven scenarios:
-% scenario 1:
+% scenario 1:dfys
     % position matrix self 1:
     % [1,3,4,5,6,7,8,22]
     % position matrix self 2:
@@ -56,11 +56,12 @@ cd('C:\Users\zuse\OneDrive\Masterarbeit\Code\')
     
 
 %% load position matrices
-load('C:\Users\zuse\OneDrive - FernUniversit?t Hagen\Masterarbeit_Data\gap_sizes\position_matrix_self_1.mat');
-load('C:\Users\zuse\OneDrive - FernUniversit?t Hagen\Masterarbeit_Data\gap_sizes\position_matrix_self_2.mat');
-load('C:\Users\zuse\OneDrive - FernUniversit?t Hagen\Masterarbeit_Data\gap_sizes\position_matrix_video_1.mat');
-load('C:\Users\zuse\OneDrive - FernUniversit?t Hagen\Masterarbeit_Data\gap_sizes\position_matrix_video_2.mat');
-
+load('C:\Users\zuse\OneDrive - FernUniversit?t Hagen\Masterarbeit_Data\gap_sizes\final_position_matrix_self_1.mat');
+load('C:\Users\zuse\OneDrive - FernUniversit?t Hagen\Masterarbeit_Data\gap_sizes\final_position_matrix_self_2.mat');
+load('C:\Users\zuse\OneDrive - FernUniversit?t Hagen\Masterarbeit_Data\gap_sizes\final_position_matrix_video_1.mat');
+load('C:\Users\zuse\OneDrive - FernUniversit?t Hagen\Masterarbeit_Data\gap_sizes\final_position_matrix_video_2.mat');
+load('C:\Users\zuse\OneDrive - FernUniversit?t Hagen\Masterarbeit_Data\gap_sizes\final_position_matrix_training_1.mat');
+load('C:\Users\zuse\OneDrive - FernUniversit?t Hagen\Masterarbeit_Data\gap_sizes\final_position_matrix_training_2.mat');
 
 %% get correct gaps for each szenario
 
@@ -80,6 +81,11 @@ pos_vid_2_2 = P_3([2,10,11,12,13,14,15,23],:);
 pos_vid_3_1 = P_3([3,16,17,18,19,20,21,24],:);
 pos_vid_3_2 = P_4([3,16,17,18,19,20,21,24],:);
 
+pos_training_self(1:8,:) = P_5([1,3,4,5,6,7,8,22],:);
+pos_training_self(9:16,:) = P_6([2,3,4,5,6,7,8,22],:);
+
+pos_training_video = P_6([1,4,5,6,7,8,9,22],:);
+
 % create gap size matrices
 gap_self_1 = pos_self_1(:,1:7)-pos_self_1(:,2:8);
 gap_self_2 = pos_self_2(:,1:7)-pos_self_2(:,2:8);
@@ -91,3 +97,6 @@ gap_vid_2_1 = pos_vid_2_1(:,1:7)-pos_vid_2_1(:,2:8);
 gap_vid_2_2 = pos_vid_2_2(:,1:7)-pos_vid_2_2(:,2:8);
 gap_vid_3_1 = pos_vid_3_1(:,1:7)-pos_vid_3_1(:,2:8);
 gap_vid_3_2 = pos_vid_3_2(:,1:7)-pos_vid_3_2(:,2:8);
+
+gap_training_self = pos_training_self(:,1:7)-pos_training_self(:,2:8);
+gap_training_video = pos_training_video(:,1:7)-pos_training_video(:,2:8);
